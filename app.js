4a7b7fbf7fbff285ca6a5abd1ad87ea98799d830
AndyM <andy.mih17@gmail.com>
2018-11-01 23:50:10 +0400

    es6

--- app.js
+++ app.js
@@ -3,12 +3,12 @@ var cheerio = require("cheerio");
 var async = require("async");
 var schedule = require('node-schedule');
 let nodemailer = require("nodemailer");
-var async = require('async');
 var each = require('async-each');
 
-var mails = ['andy.mih17@gmail.com']
 
-var transporter = nodemailer.createTransport({
+const MAILS = ['andy.mih17@gmail.com']
+
+let transporter = nodemailer.createTransport({
     service: "gmail",
     auth: {
       user: "kalachtehschedule@gmail.com",
@@ -16,43 +16,42 @@ var transporter = nodemailer.createTransport({
     }
   });
 
-function correctDate(i) {
-    return (i < 10)? "0" + i: i;
+var correctDate = (i)  => { 
+    return (i < 10)? "0" + i : i;
 }
+  
+const url = 'http://kalachteh.ru/schedule/kioskschedule.html'
+const i = schedule.scheduleJob('0 0 11 * * *', () => {
 
-var url = 'http://kalachteh.ru/schedule/kioskschedule.html'
-var i = schedule.scheduleJob('0 0 11 * * *', function() {
-
-  var j = schedule.scheduleJob('* */15 11-13 * * *', function(){
-    needle.get(url,function(err,res){
+  const j = schedule.scheduleJob('* */15 11-13 * * *', () => {
+    needle.get(url, (err,res) => {
     if(err) throw(err);
    
-    var $ = cheerio.load(res.body);
-    var date = ($('td.fs0').text());
-    var scheduleDate = date.slice(14,24);
+    let $ = cheerio.load(res.body);
+    let date = ($('td.fs0').text());
+    let scheduleDate = date.slice(14,24);
     date = date.slice(14,16);
-    var dateNow = new Date();
+    let dateNow = new Date();
     if (date != correctDate(dateNow.getDate())) {
-      var schedule = ($('tr').text());
+      let schedule = ($('tr').text());
       dateNow = new Date();
       // Поиск конца расписания на завтра
-      var endSchedule = schedule.indexOf(`Расписание на ${correctDate(dateNow.getDate())}.${correctDate(dateNow.getMonth()+1)}.${dateNow.getFullYear()}`); 
+      let endSchedule = schedule.indexOf(`Расписание на ${correctDate(dateNow.getDate())}.${correctDate(dateNow.getMonth()+1)}.${dateNow.getFullYear()}`); 
       // Оставляем в расписании только расписание на завтра
       schedule = schedule.slice(0,endSchedule);
       // Поиск нужной группы
-      var startInd = schedule.indexOf('Т33');
-      var endInd = schedule.indexOf('Т43');
-      schedule = schedule.slice(startInd, endInd);
-
-      async.each(mails, function(mail, cb) {
+      let startInd = schedule.indexOf('Т33');
+      let endInd = schedule.indexOf('Т43');
+      schedule = schedule.slice(startInd, endInd)
+      async.each(MAILS, (mail, cb) => {
         let mailOptions = {
           from: "kalachtehschedule@gmail.com",
           to: mail,
           subject: `Расписание на ${scheduleDate}! [kalachteh.ru]`,
-          text: `Привет! Держи расписание на ${scheduleDate}!\n ${schedule}`
+          text: `Привет! Держи расписание на ${scheduleDate}!\n ${schedule}`,
         };
 
-        transporter.sendMail(mailOptions, function (error, info) {
+        transporter.sendMail(mailOptions, (error, info) => {
           if (error) {
             return console.log(error);
           } else {
@@ -60,7 +59,7 @@ var i = schedule.scheduleJob('0 0 11 * * *', function() {
             cb(null, info);
           }
         });
-      }, function(error, info) {
+      }, (error, info) => {
         if (error) return console.log(error);
         console.log('done')
         j.cancel();
